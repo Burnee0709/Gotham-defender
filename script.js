cd62830e605ea7b9938685a6b0ad05c0e1378ded
Burnee0709 <Bukb333@gmail.com>
2025-11-02 11:39:44 +0800

    final product

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let currentDoor = { joker: null, batman: null };
+let score = 0;
+let isGameOver = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   const board = document.getElementById("board");
 
   for (let i = 0; i < 9; i++) {
     const door = document.createElement("div");
     door.id = i.toString();
+    door.addEventListener("click", selectDoor);
     board.appendChild(door);
   }
 
@@ -13,8 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(() => setCharecter("joker"), 1500);
   setInterval(() => setCharecter("batman"), 2000);
 });
+
 const getRandomDoorId = () => Math.floor(Math.random() * 9).toString();
 const setCharecter = (character) => {
+  if (isGameOver) return;
   const randomDoorId = getRandomDoorId();
 
   if (isDoorOccupied(randomDoorId)) return;
@@ -35,4 +41,37 @@ const clearDoor = (character) => {
   if (currentDoor[character]) {
     currentDoor[character].innerHTML = "";
   }
+};
+const selectDoor = (e) => {
+  const selectedDoor = e.target;
+  if (selectedDoor.children.lentgh === 0) return;
+  if (selectedDoor === currentDoor.joker) {
+    updateScore();
+    clearDoor("joker");
+  }
+  if (selectedDoor === currentDoor.batman) {
+    isGameOver = true;
+    const scoreText = document.getElementById("score");
+    scoreText.textContent = `GAME OVER: ${score}`;
+
+    showRestartButton();
+  }
+};
+const updateScore = () => {
+  score += 10;
+  const scoreText = document.getElementById("score");
+  scoreText.textContent = score.toString();
+};
+const showRestartButton = () => {
+  const restart = document.getElementById("restart");
+  restart.classList.add("active");
+  restart.addEventListener("click", restartGame);
+};
+const restartGame = () => {
+  isGameOver = false;
+  score = 0;
+  const scoreText = document.getElementById("score");
+  scoreText.textContent = score.toString();
+  const restart = document.getElementById("restart");
+  restart.classList.remove("active");
 };
